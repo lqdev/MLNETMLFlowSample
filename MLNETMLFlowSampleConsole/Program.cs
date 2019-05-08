a6a8a10a4b666649f381eee853d04eb575682f34
luisquintanilla3 <luis.quintanilla3@outlook.com>
2019-05-08 19:59:03 -0400

    Initial Commit

---
+++ MLNETMLFlowSampleConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLNETMLFlowSampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# ML.NET MLFlow Sample Application
+
+This application demos how to track model runs in MLFLow for models built using Automated ML in ML.NET
+
